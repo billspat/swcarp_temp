15ffbbd15d06247f675caac458243cd0a40e22eb
Pat Bills <billspat@msu.edu>
2025-03-05 07:56:30 -0500

    various data exploration in a script

---
+++ scripts/gm.R
@@ -0,0 +1,33 @@
+year_country_gdp <- 
+  gapminder %>% 
+  select(year, country, gdpPercap) %>% 
+  filter(country == "Ghana")
+
+year_continent_gdp <- 
+  gapminder %>% 
+  filter(continent == 'Asia') %>%
+  select(year, country, gdpPercap) %>%
+  rename(gdp_per_capita = gdpPercap) 
+
+
+gdp_by_continent <- gapminder %>%
+  group_by(continent, year) %>%
+  summarize(mean_gdpPercap = mean(gdpPercap))
+
+
+lifeExp_bycountry <- gapminder %>%
+  group_by(country) %>%
+  summarize(mean_lifeExp = mean(lifeExp))
+
+
+
+
+gapminder |>
+  filter(year == 2002) |> count(continent, sort = TRUE)
+
+gapminder %>% 
+  group_by(continent) %>%
+  summarize(se_le = sd(lifeExp)/sqrt(n())) |>  View()
+
+gapminder_gdp <- gapminder %>%
+  mutate(gdp = pop * gdpPercap )
